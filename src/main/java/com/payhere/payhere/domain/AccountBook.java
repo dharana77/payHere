8dee55497433638612a4626660a90f6bbe14e760
dharana <dharana7723@gmail.com>
2022-12-28 00:47:59 +0900

    feat: AccountBook 도메인 추가

---
+++ src/main/java/com/payhere/payhere/domain/AccountBook.java
@@ -0,0 +1,37 @@
+package com.payhere.payhere.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@Entity
+public class AccountBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "account_book_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String consumedName;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+
+    @Lob
+    @Column(nullable = false)
+    private String consumedDetail;
+
+    @Builder
+    public AccountBook(String consumedName, int price, String consumedDetail){
+        this.consumedName = consumedName;
+        this.price = price;
+        this.consumedDetail = consumedDetail;
+    }
+}
